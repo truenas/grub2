2047513443196b953d65504494c7ec67eff5b063
Ameer Hamza <ahamza@ixsystems.com>
2025-04-29 16:09:06 +0500

    Removing failing patch

--- pull.sh
+++ pull.sh
@@ -14,6 +14,10 @@ rm debian/patches/efi-variable-storage-minimise-writes.patch
 sed -i.bak 's/efi-variable-storage-minimise-writes.patch//' debian/patches/series
 rm debian/patches/series.bak
 
+rm debian/patches/efivar-check-that-efivarfs-is-writeable.patch
+sed -i.bak 's/efivar-check-that-efivarfs-is-writeable.patch//' debian/patches/series
+rm debian/patches/series.bak
+
 cp zpool-degraded-vdev.patch debian/patches
 echo 'zpool-degraded-vdev.patch' >>debian/patches/series
 
