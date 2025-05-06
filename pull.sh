2a01d14c80070e19c65b2804ed92562dbed28966
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-05-07 00:33:52 +0500

    Add comment

--- pull.sh
+++ pull.sh
@@ -10,6 +10,9 @@ wget http://deb.debian.org/debian/pool/main/g/grub2/grub2_$VERSION.orig.tar.xz
 tar xf grub2_$VERSION.orig.tar.xz --strip 1
 rm grub2_$VERSION.orig.tar.xz
 
+# At the time we bumped version to 2.12-1, we were not seeing efivar.c present in the debian
+# source at https://deb.debian.org/debian/pool/main/g/grub2/grub2_2.12.orig.tar.xz
+# which resulted in this patch failing and hence it has been removed
 sed -i '/efivar-check-that-efivarfs-is-writeable.patch/d' debian/patches/series
 rm -f debian/patches/efivar-check-that-efivarfs-is-writeable.patch
 
